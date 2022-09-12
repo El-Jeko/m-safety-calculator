d6b0ad5f268993cd9ce312a3b0497caf816bce6c
El-Jeko <113358367+El-Jeko@users.noreply.github.com>
2022-09-12 17:31:34 +0300

    Update mail.php

--- mail.php
+++ mail.php
@@ -12,26 +12,26 @@ $email = $_POST['email'];
 $phone = $_POST['phone-number'];
 $totalCost = $_POST['total-cost'];
 
-$mail->SMTPDebug = 3;                               // Enable verbose debug output
+// $mail->SMTPDebug = 3;
 
-$mail->isSMTP();                                      // Set mailer to use SMTP
-$mail->Host = 'ssl://smtp.mail.ru';                                                                                              // Specify main and backup SMTP servers
-$mail->SMTPAuth = true;                               // Enable SMTP authentication
-$mail->Username = 'info@m-safety.ru'; // Ваш логин от почты с которой будут отправляться письма
-$mail->Password = 'NpYUaPi33a*c'; // Ваш пароль от почты с которой будут отправляться письма
-$mail->SMTPSecure = 'ssl';                            // Enable TLS encryption, `ssl` also accepted
-$mail->Port = 465; // TCP port to connect to / этот порт может отличаться у других провайдеров
+$mail->isSMTP();
+$mail->Host = 'ssl://smtp.mail.ru'; 
+$mail->SMTPAuth = true; 
+$mail->Username = 'info@m-safety.ru';
+$mail->Password = 'password';
+$mail->SMTPSecure = 'ssl';
+$mail->Port = 465;
 
-$mail->setFrom('info@m-safety.ru'); // от кого будет уходить письмо?
-$mail->addAddress('info@m-safety.ru');     // Кому будет уходить письмо
-$mail->addAddress($email);     // Кому будет уходить письмо
-//$mail->addAddress('ellen@example.com');               // Name is optional
+$mail->setFrom('info@m-safety.ru');
+$mail->addAddress('info@m-safety.ru');
+$mail->addAddress($email);
+//$mail->addAddress('ellen@example.com');
 //$mail->addReplyTo('info@example.com', 'Information');
 //$mail->addCC('cc@example.com');
 //$mail->addBCC('bcc@example.com');
-//$mail->addAttachment('/var/tmp/file.tar.gz');         // Add attachments
-//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');    // Optional name
-$mail->isHTML(true);                                  // Set email format to HTML
+//$mail->addAttachment('/var/tmp/file.tar.gz');
+//$mail->addAttachment('/tmp/image.jpg', 'new.jpg');
+$mail->isHTML(true);
 
 $mail->Subject = 'КАЛЬКУЛЯТОР M-SAFETY';
 $mail->Body    = '<strong>Количество камер: </strong>' . $cameraCount . '<br><strong>Размер архива: </strong>' . $archiveCount . '<br><strong>Стоимость оборудования: </strong>' . $cameraCost . '<br><strong>Стоимость работ: </strong>' . $workCost . '<br><strong>Почта: </strong>' . $email . '<br><strong>Телефон: </strong>' . $phone . '<br><strong>Итого стоимость: </strong>' . $totalCost;
